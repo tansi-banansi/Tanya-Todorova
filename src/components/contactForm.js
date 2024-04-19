fefbbe84d68d650bf3416dd59cd6b14a1fe502e8
Tanya <tanyatodorovaaa@gmail.com>
2024-04-19 14:55:59 +0100

    form changes

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -3,19 +3,19 @@ import { Form, Row , Button} from "react-bootstrap";
 
 function ContactForm(){
     return(
-        <Form name="contact" method="POST" data-netlify="true" >
+        <Form name="contact" method="POST" data-netlify="true" netlify>
             <Row>
                 <Form.Group controlId="formGroupName">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Name" id="name" name="name"/>
+                    <Form.Control type="text" placeholder="Name" name="name" />
                 </Form.Group>
                 <Form.Group controlId="formGroupEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="text" placeholder="Enter email" id="email" name="email" />
+                    <Form.Control type="email" placeholder="Enter email" name="email" />
                 </Form.Group>
                 <Form.Group controlId="formGroupMessage">
                     <Form.Label>Message</Form.Label>
-                    <Form.Control as="textarea" placeholder="Message"/>
+                    <Form.Control as="textarea" placeholder="Message" name="message" />
                 </Form.Group>
                 <Button type="submit">
                     Submit
